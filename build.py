136f809a66543b18e1f4abee4072c6430675b8f5
Aaron Lidman <aaronlidman@gmail.com>
2013-07-25 16:29:02 -0700

    Two databases, no more buffer

--- build.py
+++ build.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 
-# converts a geojson file, from ogr2ogr, to a sqlite database with individuals features as rows
+# converts a geojson file with polygons to a sqlite database with individuals features as rows
 # run: python build.py file.geojson
-# creates a sqlite db, named file.sqlite
-
-# TODO:
-#   size limitation, figure out the units, use bounds instead
-
+# creates two sqlite databases, named [file].sqlite and [file]-difficult.sqlite
+    # [file] is taken from the source file
 import json
 import sqlite3
 from shapely.geometry import asShape, mapping
@@ -36,7 +33,6 @@ def isEditable(geo):
     # items that are easily editable for leaflet
     # mirrors isEditable() in osmly.item.js
     if geo.geom_type == 'Polygon' and geo.interiors:
-        # rare but happens
         return False
     elif geo.geom_type == 'MultiPolygon':
         return False
@@ -47,26 +43,31 @@ def isEditable(geo):
 
 data = open(args['source'])
 data = json.load(data)
+dbName = args['source'].split('.')
 
-sqlite = args['source'].split('.')
-sqlite = sqlite[0] + '.sqlite'
+editableDB = dbName[0] + '.sqlite'
+edit_conn = sqlite3.connect(editableDB)
+edit_c = edit_conn.cursor()
+edit_c.execute("DROP TABLE IF EXISTS osmly")
+edit_c.execute('''CREATE TABLE osmly (id INT, geo TEXT, osc TEXT, problem TEXT, done TEXT)''')
+edit_conn.commit()
 
-conn = sqlite3.connect(sqlite)
-c = conn.cursor()
-c.execute("DROP TABLE IF EXISTS osmly")
-c.execute('''CREATE TABLE osmly (id INT, geo TEXT, osc TEXT, problem TEXT, done TEXT)''')
-conn.commit()
+difficultDB = dbName[0] + '-difficult.sqlite'
+diff_conn = sqlite3.connect(difficultDB)
+diff_c = diff_conn.cursor()
+diff_c.execute("DROP TABLE IF EXISTS osmly")
+diff_c.execute('''CREATE TABLE osmly (id INT, geo TEXT, osc TEXT, problem TEXT, done TEXT)''')
+diff_conn.commit()
 
 count = 0
-difficult = 0
 
 for feature in data['features']:
     geo = asShape(feature['geometry'])
-    bounds = geo.buffer(0.001).bounds
+    bounds = geo.bounds
     geoarea = geo.area
     editable = isEditable(geo)
 
-    # we want to use simplify() with False param because it's faster
+    # we want to use simplify() with False because it's faster
     # but it occasionally deletes all nodes and that upsets mapping()
     try:
         simple = geo.simplify(args['simplify'], False)
@@ -76,20 +77,20 @@ for feature in data['features']:
         geo = mapping(simple)
 
     feature['properties']['id'] = count
-    feature['properties']['buffer_bounds'] = bounds
+    feature['properties']['bounds'] = bounds
     feature['geometry']['coordinates'] = geo['coordinates']
 
+    statement = "INSERT INTO osmly VALUES(?, ?, ?, ?, ?);"
+
     if editable:
-        statement = "INSERT INTO osmly VALUES(?, ?, ?, ?, ?);"
-        c.execute(statement, (count, json.dumps(feature), '', '', ''))
-        conn.commit()
-        count = count + 1
+        edit_c.execute(statement, (count, json.dumps(feature), '', '', ''))
+        edit_conn.commit()
     else:
-        # put it into some other file
-        # print str(count) + ': ' + json.dumps(feature)
-        difficult = difficult + 1
+        diff_c.execute(statement, (count, json.dumps(feature), '', '', ''))
+        diff_conn.commit()
+    count = count + 1
 
 
-print 'leaflet editable: ' + str(count)
-print 'too difficult: ' + str(difficult)
-conn.close()
+print '# of items: ' + str(count)
+edit_conn.close()
+diff_conn.close()
