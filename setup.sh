7e7f87a9d31583d3c6b6279013aa57756f29bbae
Aaron Lidman <aaronlidman@gmail.com>
2013-08-26 19:04:40 -0700

    Moving server related stuff to osmly-server repo

--- setup.sh
+++
@@ -1,6 +0,0 @@
-apt-get -y update
-apt-get -y upgrade
-apt-get install vim git htop python-pip python-dev build-essential nginx
-pip install uwsgi flask
-
-ln -sf /parks-project/nginx.conf /etc/nginx/sites-enabled/default
