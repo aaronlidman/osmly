ed879b0bbb98480509bdfeb1331c5b029f282bd9
Aaron Lidman <aaronlidman@gmail.com>
2013-09-12 12:18:42 -0700

    Hooked up 'simplify' argument, added 'names' argument and column

--- build.py
+++ build.py
@@ -25,6 +25,10 @@ parser.add_argument(
     help='Simplification tolerance.',
     type=float,
     default=0.0001)
+parser.add_argument(
+    '--names',
+    help='The name column (or technically property)',
+    type=str)
 
 args = vars(parser.parse_args())
 
@@ -35,7 +39,6 @@ def isEditable(geo):
     if geo.geom_type == 'Polygon' and geo.interiors:
         # geojson makes a distinction between multipolygons and polygon w/ a hole
         # osm does not
-        # actually a holey polygon, 'complex polygon'
         return 'multipolygon'
     elif geo.geom_type == 'MultiPolygon':
         return 'multipolygon'
@@ -47,12 +50,15 @@ def isEditable(geo):
 
 
 def trunc_bounds(bounds):
-    out = [1, 2, 3, 4]
-    out[0] = float('{0:.5f}'.format(bounds[0]))
-    out[1] = float('{0:.5f}'.format(bounds[1]))
-    out[2] = float('{0:.5f}'.format(bounds[2]))
-    out[3] = float('{0:.5f}'.format(bounds[3]))
-    return out
+    return [
+        _trunc(bounds[0]),
+        _trunc(bounds[1]),
+        _trunc(bounds[2]),
+        _trunc(bounds[3])
+    ]
+
+def _trunc(dec):
+    return float('{0:.5f}'.format(dec))
 
 data = open(args['source'])
 data = json.load(data)
@@ -63,7 +69,7 @@ db_conn.isolation_level = None
     # autocommit
 db_c = db_conn.cursor()
 db_c.execute('DROP TABLE IF EXISTS osmly')
-db_c.execute('CREATE TABLE osmly (id INTEGER PRIMARY KEY, geo TEXT, remote TEXT,' +
+db_c.execute('CREATE TABLE osmly (id INTEGER PRIMARY KEY, name TEXT, geo TEXT, remote TEXT,' +
              'problem TEXT, submit TEXT, comments TEXT, user TEXT, time INT, done INT)')
 
 count = 0
@@ -72,23 +78,27 @@ for feature in data['features']:
     geo = asShape(feature['geometry'])
     bounds = trunc_bounds(geo.bounds)
     problem = isEditable(geo)
+    name = ''
 
-    # simplify(x, False) is faster, turns off preserve_topology
+    # simplify(x, False) is significantly quicker, turns off preserve_topology
     # but it occasionally deletes all nodes and that upsets mapping()
     try:
-        simple = geo.simplify(0.0001, False)
+        simple = geo.simplify(args['simplify'], False)
         geo = mapping(simple)
     except:
-        simple = geo.simplify(0.0001, True)
+        simple = geo.simplify(args['simplify'], True)
         geo = mapping(simple)
+
+    if 'names' in args and args['names'] in feature['properties']:
+        name = feature['properties'][args['names']]
 
     feature['properties']['bounds'] = bounds
     feature['geometry']['coordinates'] = geo['coordinates']
     feature['properties']['id'] = count
-    statement = 'INSERT INTO osmly VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);'
+    statement = 'INSERT INTO osmly VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);'
 
     db_c.execute(statement, (
-        count, json.dumps(feature), '', problem, '', '', '', 0, 0))
+        count, name, json.dumps(feature), '', problem, '', '', '', 0, 0))
     count = count + 1
 
 print str(count) + ' items'
