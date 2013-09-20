2f24ba90ddacadaa06b617efe1bf47547df37c1a
Aaron Lidman <aaronlidman@gmail.com>
2013-09-20 13:55:25 -0700

    Some API documentation, unfinished

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ apt-get -y upgrade
 apt-get -y install vim git htop python-pip python-dev build-essential nginx libgeos-dev
 pip install uwsgi flask shapely
 
-ln -sf nginx.conf /etc/nginx/sites-enabled/default
+cp nginx.conf /etc/nginx/sites-enabled/osmly
 /usr/local/bin/uwsgi --socket 127.0.0.1:4242 --module server --callable app --daemonize log.txt
 
 service nginx restart
